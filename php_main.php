003e2f9c43179f6324dd49212a8ed48491e77d3d
viperphase1 <thomasgriffiths11@hotmail.com>
2016-04-27 11:19:42 -0600

    Update and rename php_main to php_main.php

---
+++ php_main.php
@@ -0,0 +1,16 @@
+<?php
+$ curl -s http://getcomposer.org/installer | php
+$ php composer.phar install
+{
+    "require": {
+        "knplabs/github-api": "~1.4"
+    }
+}
+
+require_once 'vendor/autoload.php';
+$user = "viperphase1";
+$org = "tmgOrganization1";
+$client = new \Github\Client();
+$repositories = $client->api('user')->repositories('ornicar');
+print_r($repositories);
+?>
